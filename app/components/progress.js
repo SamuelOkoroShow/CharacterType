72b0350d93b1b1d2edf89b0e4be1f9d6c8a86505
Sam Okoro <s.okoro07@gmail.com>
2016-10-02 15:46:22 -0500

    Progress View

--- app/components/progress.js
+++ app/components/progress.js
@@ -10,6 +10,7 @@ import {
   StyleSheet,
   Text,
   Image,
+  ListView,
   Dimensions,
   TouchableOpacity,
   View
@@ -47,46 +48,67 @@ var missions = [
   progress: 33
 }
 ]
+const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+
 export default class Progress extends Component {
   constructor(props){
     super(props)
 
     this.state={
-      fill:50
+      fill:50,
+      dataSource: ds.cloneWithRows(missions),
     }
   }
 
-  render() {
-    return (
-      <Image resizeMode="contain" source={require('../images/background.jpg')} style={styles.container}>
-      <Nav name = "PROGRESS" {...this.props}/>
-        <View style={{flexDirection:'row', justifyContent:'space-between', margin:15, marginBottom:-30}}>
+  eachMission(x){
+    return(
+      <View>
+      <View style={{flexDirection:'row', justifyContent:'space-between', margin:15, marginBottom:-30}}>
         <Image source={require('../images/rally.png')} resizeMode="contain" style={{width:100, height:100, alignSelf:'center'}} />
         <Image source={require('../images/aid.png')} resizeMode="contain" style={{width:100, height:100, alignSelf:'center'}} />
         </View>
-        <Image source={require('../images/mission1.png')} resizeMode="contain" style={{width:160, height:160, alignSelf:'center'}} />
-      <View style={{backgroundColor:'rgba(255,255,255,0)', marginBottom:-30, marginTop:10, flexDirection:'row', alignItems:'center',}}>
+        <Image source={x.image} resizeMode="contain" style={{width:160, height:160, alignSelf:'center'}} />
+      <View style={{backgroundColor:'rgba(255,255,255,0)', marginTop:-30, flexDirection:'row', alignItems:'center',}}>
       <View style={{backgroundColor:'rgba(255,255,255,0)', flexDirection:'row', alignItems:'center'}}>
       <AnimatedCircularProgress
       style={{margin:10, alignItems:'center', justifyContent:'center'}}
   size={70}
   width={3}
-  fill={this.state.fill}
+  fill={x.progress}
   tintColor="#00e0ff"
   backgroundColor="#999">
 
    {
     (fill) => (
         <Text style={{color:'#00e0ff', fontSize:18, fontWeight:'300', position:"absolute", top:23, left:20}}>
-        { this.state.fill }%
+        { x.progress }%
       </Text>
     )
   }
   </AnimatedCircularProgress>
   <Text style={{color:'#333', fontSize:11}}>MISSION {'\n'}PROGRESS</Text>
   </View>
-  <View></View>
+  <View style={{width:200, shadowColor: "#000000", shadowOpacity: 0.3, shadowRadius: 3, shadowOffset: { height: 1, width: 0 }}}>
+  <View style={{ backgroundColor:'#333'}}>
+<Text style={{color:'#fff', margin:5}}>DEATH DEFIANT</Text>
   </View>
+  <View style={{flex:4, backgroundColor:'#fff'}}>
+<Text style={{margin:5}}>{missions[0].deets}</Text>
+  </View>
+  </View>
+  </View>
+  </View>
+  )
+  }
+  render() {
+    return (
+      <Image resizeMode="contain" source={require('../images/background.jpg')} style={styles.container}>
+      <Nav name = "PROGRESS" {...this.props}/>
+         <ListView
+          dataSource={this.state.dataSource}
+          renderRow={(rowData) => this.eachMission(rowData)}
+          style={{marginTop:5}}
+        />
       </Image>
     );
   }
