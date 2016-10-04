991b62069775ad9d3f7a28878afdbd4ab788ff59
Sam Okoro <s.okoro07@gmail.com>
2016-10-04 13:19:16 -0500

    Moving backgrounds

--- app/components/campaign.js
+++ app/components/campaign.js
@@ -10,17 +10,54 @@ import {
   StyleSheet,
   Text,
   Image,
+  ScrollView,
+  ListView,
   Dimensions,
   TouchableOpacity,
   View,
   Animated
 } from 'react-native';
 
+var missions = [
+{
+  image: require('../images/raid.png'),
+  name:'Bandit Raid',
+  info:'Orem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur cursus lobortis arcu, vel porta erat tempor sed. Sed pulvinar ultricies purus, scelerisque rutrum enim iaculis quis'
+},
+{
+  image: require('../images/death.png'),
+  name:'Death Match',
+  info:'Orem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur cursus lobortis arcu, vel porta erat tempor sed. Sed pulvinar ultricies purus, scelerisque rutrum enim iaculis quis'
+},
+{
+  image: require('../images/boozeRun.png'),
+  name: "Booze Run",
+  info: 'Orem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur cursus lobortis arcu, vel porta erat tempor sed. Sed pulvinar ultricies purus, scelerisque rutrum enim iaculis quis'
+},
+{
+  name: "Shark Raid",
+  image: require('../images/sharkRaid.png'),
+  info: 'Orem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur cursus lobortis arcu, vel porta erat tempor sed. Sed pulvinar ultricies purus, scelerisque rutrum enim iaculis quis'
+},{
+  name:'Sail',
+  image: require('../images/sail.png'),
+  info:'Orem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur cursus lobortis arcu, vel porta erat tempor sed. Sed pulvinar ultricies purus, scelerisque rutrum enim iaculis quis'
+}
+]
 var {height, width} = Dimensions.get('window');
 var hasAnimated = false;
 import Nav from './widgets/nav'
+const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
 
 export default class Campaign extends Component {
+  constructor(props){
+    super(props)
+     this.state={
+      fill:50,
+      dataSource: ds.cloneWithRows(missions),
+    }
+
+  }
     componentWillMount(){
     this._animatedValue = new Animated.Value(0);
   }
@@ -28,16 +65,41 @@ export default class Campaign extends Component {
  componentDidMount() {
     Animated.timing(this._animatedValue, {
         toValue: -900,
-        duration: 41500
+        duration: 51500
     }).start();
   }
 
+  eachMission(x){
+    return(
+      <View>
+<View style={{height:100, flexDirection:'row', width:360, margin:3, alignItems:'center', backgroundColor:'rgba(0,0,0,0.54'}}>
+      <Image source={x.image} style={{height:40, margin:10, width:40}} resizeMode='contain' />
+      <View style={{width:280}}>
+      <Text style={{color:'#fff', marginBottom:5, fontWeight:'600', textDecorationLine:'underline'}}>{x.name}</Text>
+      <Text style={{color:'#fff', fontSize:12}}>{x.info}</Text>
+      </View>
+      </View>
+      
+      </View>
+      )
+  }
 
   render() {
     return (
       <View style={styles.container}>
       <Animated.Image source={require('../images/longBack.jpg')} resizeMode="contain" style={{position:'absolute', left:0, top:0, width:width*4+100, height:height, transform: [{translateX: this._animatedValue}] }} />
       <Nav name="Campaigns" {...this.props} />
+      <View style={{alignItems:'center'}}>
+      <View style={{height:60, width:360, margin:3, marginTop:10, alignItems:'center', justifyContent:'center', backgroundColor:this.props.user.color}}>
+      <Text style={{color:'#fff',fontWeight:'500'}}>AVAILABLE MISSIONS</Text>
+      <Text style={{color:'#fff',fontWeight:'400', fontSize:11}}>(COMMUNITY BUILT)</Text>
+      </View>
+      <ListView
+          dataSource={this.state.dataSource}
+          renderRow={(rowData) => this.eachMission(rowData)}
+          style={{marginTop:5}}
+        />
+      </View>
       </View>
     );
   }
